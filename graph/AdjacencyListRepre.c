4bf3740e961c88d64f793b8977aa3558aa530621
Dhananjay137 <dhananjayrathod006@gmail.com>
2026-07-31 11:22:30 +0530

    implement breadth-first search (BFS) algorithm for graph traversal

--- graph/AdjacencyListRepre.c
+++ graph/AdjacencyListRepre.c
@@ -10,6 +10,8 @@ void create_graph(Node *Graph[], int no_of_nodes);
 void display_adj_list(Node *Graph[], int no_of_nodes);
 void add_edge(Node *Graph[], int source, int destination);
 Node *create_node(int data);          // returns pointer
+void bfs(Node *Graph[], int no_of_nodes, int visited[]);
+void enqueue(int *front, int *rear, int arr[], int val);
 
 int main() {
   int total_no_of_nodes, total_no_of_edges;
@@ -43,6 +45,12 @@ int main() {
   printf("\nAdjacency List:\n");
   display_adj_list(Graph, total_no_of_nodes);
 
+  int visited[total_no_of_nodes];
+  for (int i = 0; i < total_no_of_nodes; i++)
+    visited[i] = 0;
+  
+  bfs(Graph, total_no_of_nodes, visited);
+
   // Free memory
   for (int i = 0; i < total_no_of_nodes; i++) {
     Node *curr = Graph[i];
@@ -66,6 +74,61 @@ Node *create_node(int data) {
   new_node->next = NULL;
 
   return new_node;
+}
+
+void enqueue(int *front, int *rear, int arr[], int val) {
+  if (*front == -1 && *rear == -1)
+    *front = *rear = 0;
+  else
+    (*rear)++;
+
+  arr[*rear] = val;
+}
+
+void bfs(Node *Graph[], int no_of_nodes, int visited[]) {
+  int source = 0;
+  int queue[no_of_nodes], front = -1, rear = -1;
+  int destination;
+
+  printf("Enter destination (0 to %d): ",no_of_nodes-1);
+  scanf("%d",&destination);
+
+  // 1. Enqueue source and mark visited
+  enqueue(&front,&rear,queue,source);
+  visited[source] = 1;
+  
+  // 2. Loop runs until the queue is completely empty
+  while (front <= rear && front != -1) {
+
+    // Get and dequeue Current Vertex
+    int current_vertex = queue[front++];
+
+    // Check current node == destination
+    if (current_vertex == destination) {
+      printf("\nFound destination %d!\n", destination);
+      return;
+    }
+
+    // 3. Move ptr to the neighbors of the DEQUEUED node
+    Node *ptr = Graph[current_vertex];
+
+    while (ptr != NULL) {
+      // Enqueue unvisited node & mark them visited
+      if (visited[ptr->data] == 0) {
+        enqueue(&front,&rear,queue,ptr->data);
+        visited[ptr->data] = 1;
+      }
+    
+      ptr = ptr->next;
+    }
+  }
+
+  // printf("front: %d, rear: %d\n",front,rear);
+  // for(int i=0; i<no_of_nodes; i++)
+  //   printf("%d ",queue[i]);
+  printf("\nDestination %d not reachable from source.\n", destination);
+
+  return;
 }
 
 void create_graph(Node *Graph[], int no_of_nodes) {
